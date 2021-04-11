6dd783c21a25919cb2f3774cdaa27ad69979483c
fackaccount <82316644+fackaccount@users.noreply.github.com>
2021-04-11 17:12:11 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/fackaccount/Kazuto && cd Kazuto && python3 -m tg_bot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
+system("python3 -m tg_bot")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
